abd34d740e440d3faf877445b3c59d24e298e4fa
perfectrider <konsgrebenev@gmail.com>
2022-08-16 14:55:07 +0300

    Добавил класс Article и настройки для него, выполнил миграции

--- fitnessblog/fitnessblog/urls.py
+++ fitnessblog/fitnessblog/urls.py
@@ -13,7 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
+
+from fitnessblog import settings
 from main.views import *
 from django.urls import path, include
 
@@ -22,5 +25,8 @@ urlpatterns = [
     path('', include('main.urls')),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = pageNotFound
--- fitnessblog/main/models.py
+++ fitnessblog/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=250)
+    content = models.TextField(blank=True)
+    image = models.ImageField(upload_to='images/')
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
