4cd6b0e249931c2cc0e6f9f35debb5185da73d70
perfectrider <konsgrebenev@gmail.com>
2022-08-20 15:13:11 +0300

    Поправил переход на страницу about

--- fitnessblog/main/urls.py
+++ fitnessblog/main/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from . import views
-# from .views import article_show
 
 urlpatterns = [
     path('', views.index, name='Главная'),
-    path('about/', views.about, name='О нас'),
-    path('article/<int:article_id>/', views.ArticlesDetailView.as_view(), name='article_detail'),
+    path('about', views.about, name='О нас'),
+    path('<int:pk>', views.ArticlesDetailView.as_view(), name='article_detail'),
 ]
--- fitnessblog/main/views.py
+++ fitnessblog/main/views.py
@@ -7,23 +7,23 @@ from main.models import Article
 def index(request):
     articles = Article.objects.order_by('-time_create')
     return render(request, 'main/index.html',
-                  {'title': 'Титл: Главная', 'articles': articles})
+                  {'title': 'Фитнес блог', 'articles': articles})
 
 def about(request):
     return render(request, 'main/about.html',
-                  {'title': 'Титл: Обо мне'})
+                  {'title': 'Обо мне'})
 
 # def article_show(request, article_id):
 #     if request.GET:
 #         print(request.GET)
 #     return HttpResponse(f'Читать полностью...{article_id}')
 
-
 class ArticlesDetailView(DetailView):
     # Отдельное окно для полного просмотра статьи
     model = Article
     template_name = 'main/article.html'
     context_object_name = 'article'
+    title = Article.title
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('Такой страницы не существует!')
