3d42ee40ee2ec1c8cae405cad75d7578718e178c
perfectrider <konsgrebenev@gmail.com>
2022-08-22 11:39:55 +0300

    Переписал ссылки в templates. Добавил фильтры в article.html и index.html

--- fitnessblog/main/urls.py
+++ fitnessblog/main/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='Главная'),
-    path('about', views.about, name='О нас'),
+    path('about', views.about, name='Обо мне'),
     path('<int:pk>', views.ArticlesDetailView.as_view(), name='article_detail'),
 ]
