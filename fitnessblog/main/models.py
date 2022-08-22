18dc3a6b9cfcc76717aee6a61fad164fe44f785a
perfectrider <konsgrebenev@gmail.com>
2022-08-22 14:30:29 +0300

    Подключил и настроил админку.

--- fitnessblog/main/models.py
+++ fitnessblog/main/models.py
@@ -3,23 +3,33 @@ from django.db.models import PROTECT
 
 
 class Article(models.Model):
-    title = models.CharField(max_length=250)
-    content = models.TextField(blank=True)
-    image = models.ImageField(upload_to='images/')
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=True)
+    title = models.CharField(max_length=250, verbose_name='Название статьи')
+    content = models.TextField(blank=True, verbose_name='Текст')
+    image = models.ImageField(upload_to='images/', verbose_name='Изображение')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Создано')
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Отредактировано')
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
     # добавил ключ для связи с таблицей категорий
     category = models.ForeignKey('Category', on_delete=PROTECT, null=True)
 
     def __str__(self):
         return self.title
 
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+        ordering = ['-time_create']
+
 class Category(models.Model):
     '''Модель, определяющая название категории статей,
     по которым будут группироваться статьи, с присвоенными
     им категориям'''
-    name = models.CharField(max_length=100, db_index=True)
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Категория')
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['id']
