48ad6cc6c950e5c45a87ae48b480432bba1efe79
perfectrider <konsgrebenev@gmail.com>
2022-08-29 17:27:58 +0300

    привязал добавление статьи к авторизации. прописал пагинацию

--- fitnessblog/main/urls.py
+++ fitnessblog/main/urls.py
@@ -1,9 +1,13 @@
+from django.contrib.auth import login
 from django.urls import path
 from . import views
+from .views import RegisterUser
 
 urlpatterns = [
     path('', views.MainPage.as_view(), name='Главная'),
     path('about', views.AboutPage.as_view(), name='Обо мне'),
     path('addarticle', views.AddArticle.as_view(), name='Добавить статью'),
+    path('login', login, name='login'),
+    path('register', RegisterUser.as_view(), name='register'),
     path('<slug:slug>', views.ArticlesDetailView.as_view(), name='article_detail'),
 ]
